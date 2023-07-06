c915560508a4b3521449cc972bd875efa570f6fc
shardhayv <shardhayvatshyayan7@gmail.com>
2023-07-06 16:46:22 +0545

    Variables using var and let

---
+++ variables.swift
@@ -0,0 +1,20 @@
+import Foundation;
+
+let myFirstName = "Shardhay"
+let myLastName = "Vatshyayan"
+
+
+//let is immutable 
+// myFirstName = myLastName (not possible with let)
+
+// var is mutable
+var names = [
+    myLastName, myFirstName
+]
+
+names.append("BN")
+names.append("Thakur")
+
+
+
+
