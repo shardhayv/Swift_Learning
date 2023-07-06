446d74188af5e30852fb7f3866b3a294f5aed826
shardhayv <shardhayvatshyayan7@gmail.com>
2023-07-06 18:12:23 +0545

    if-else with AND and OR operators

---
+++ if_else.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+let myName = "myName"
+let myEmail = "myEmail"
+let yourName = "yourName"
+let yourEmail = "yourEmail"
+
+
+if myName ==  "myName" && myEmail == "hello@gmail.com" && yourName == "yourName" || yourEmail == "yourEmail"{
+    print("All the and are calculated logically at once and then the logic for or is calculated and it becomes true even if one logic for myEmail if False")
+}
