22075f0a2d1542397362630cc297138f2ab4d1e5
shardhayv <shardhayvatshyayan7@gmail.com>
2023-07-06 17:06:37 +0545

    making let mutable using NSMutableArray

--- variables.swift
+++ variables.swift
@@ -7,6 +7,25 @@ let myLastName = "Vatshyayan"
 //let is immutable 
 // myFirstName = myLastName (not possible with let)
 
+// Exceptions are there like NSMutableArray where let acts mutable
+let mutableNames = NSMutableArray(
+    array: [
+        "Hello",
+        "World", 
+    ]
+)
+
+func changeImmutableArray(_ array: NSArray){
+    let newArray = array as! NSMutableArray
+    newArray.add("Okay")
+
+
+}
+
+changeImmutableArray(mutableNames)
+
+print(mutableNames)
+
 // var is mutable
 var names = [
     myLastName, myFirstName
@@ -18,3 +37,4 @@ names.append("Thakur")
 
 
 
+print(names)
